b2ba7d59437335b55bc317059b9baca47ce25013
danilodhm <danilopw95@gmail.com>
2025-06-17 22:37:53 -0300

    feat: metodo de uso getter e setter com classe definidas 02

--- get-started/src/Person.java
+++ get-started/src/Person.java
@@ -3,10 +3,21 @@ public class Person {
     String name;
 
     Integer age;
- 
+       
+    public String getName() {
+        return name;
+    }
 
-    
+    public Integer getAge() {
+        return age;
+    }
 
-    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
 }
